89f0beff72648b47344e4a7b2f0c43bcbfd6c6be
Olli-Matti Saario <osaa@dyn-2-12.lan.futurice.org>
2014-07-03 16:29:39 +0300

    json

--- web.js
+++ web.js
@@ -6,6 +6,7 @@ var app = express();
 app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
+res.set('Content-Type', 'application/json');
   res.send('{"futuFeedbackItems":[{"topic":"Design","question":"Koska"},{"topic":"Design","question":"Miksi"},{"topic":"Design","question":"Why"},{"topic":"Development","question":"Koska"},{"topic":"Development","question":"Miksi"},{"topic":"Development","question":"Why"},{"topic":"Development","question":"Koska"},{"topic":"Development","question":"Miksi"},{"topic":"Design","question":"Why"},{"topic":"Development","question":"Koska"},{"topic":"Communication","question":"Miksi"},{"topic":"Development","question":"Why"},{"topic":"Support","question":"Koska"},{"topic":"Random","question":"Miksi"},{"topic":"Nothing","question":"Why"}]}');
 });
 
