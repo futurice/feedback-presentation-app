9585a7d243e48734eb07e73c097264dffd14f00f
Olli-Matti Saario <osaa@dyn-2-12.lan.futurice.org>
2014-07-07 12:09:39 +0300

    less item2s

--- web.js
+++ web.js
@@ -7,7 +7,7 @@ app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 res.set('Content-Type', 'application/json');
-  res.send('{"futuFeedbackItems":[{"topic":"Design","question":"Koska"},{"topic":"Design","question":"Miksi"},{"topic":"Design","question":"Why"},{"topic":"Development","question":"Koska"},{"topic":"Development","question":"Miksi"},{"topic":"Development","question":"Why"}}]}');
+  res.send('{"futuFeedbackItems":[{"topic":"Design","question":"Koska"},{"topic":"Design","question":"Miksi"},{"topic":"Design","question":"Why"},{"topic":"Development","question":"Koska"},{"topic":"Development","question":"Miksi"},{"topic":"Development","question":"Why"}]}');
 });
 
 var port = Number(process.env.PORT || 5000);
