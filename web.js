83ce9947c15b76e643fe6adf994a393e867b9ce0
Olli-Matti Saario <osaa@dyn-3-250.lan.futurice.org>
2014-07-08 17:18:37 +0300

    some experimental views

--- web.js
+++ web.js
@@ -7,87 +7,127 @@ var app = express();
 
 app.use(logfmt.requestLogger());
 
-function insertbsdata(db)
-{
-  
-  db.insert({ question: "Why?", topic: "Development", type:"question"});
-  db.insert({ question: "What?", topic: "Design", type:"question"});
-  db.insert({ question: "What?", topic: "Design", type:"question"});
-  db.insert({ question: "What?", topic: "Design", type:"question"});
-  db.insert({ question: "Where?", topic: "Development", type:"question"});
-  db.insert({ question: "When?", topic: "Design", type:"question"});
-  db.insert({ question: "What?", topic: "Design", type:"question"});
-  db.insert({ question: "What?", topic: "Design", type:"question"});
-  db.insert({ question: "What?", topic: "Communication", type:"question"});
-  db.insert({ question: "What?", topic: "Communication", type:"question"});
-}
+
 
 //MIGRATION
 
+function migration()
+{
+  function insertbsdata(db)
+  {
 
-// clean up the database we created previously
-nano.db.destroy('futufeedback', function() {
+    db.insert({ question: "Why?", topic: "Development", type:"question"});
+    db.insert({ question: "What?", topic: "Design", type:"question"});
+    db.insert({ question: "What?", topic: "Design", type:"question"});
+    db.insert({ question: "What?", topic: "Design", type:"question"});
+    db.insert({ question: "Where?", topic: "Development", type:"question"});
+    db.insert({ question: "When?", topic: "Design", type:"question"});
+    db.insert({ question: "What?", topic: "Design", type:"question"});
+    db.insert({ question: "What?", topic: "Design", type:"question"});
+    db.insert({ question: "What?", topic: "Communication", type:"question"});
+    db.insert({ question: "What?", topic: "Communication", type:"question"});
+
+    db.insert({ question: "What?", project: "jokuprokkis", answer: "3", type:"answer"});
+    db.insert({ question: "Where?", project: "jokuprokkis", answer: "2", type:"answer"});
+    db.insert({ question: "What?", project: "jokuprokkis", answer: "3", type:"answer"});
+    db.insert({ question: "What?", project: "jokuprokkis", answer: "2", type:"answer"});
+
+
+  }
+  // clean up the database we created previously
+  nano.db.destroy('futufeedback', function() {
   // create a new database
   nano.db.create('futufeedback', function() {
     // specify the database we are going to use
     var db = nano.use('futufeedback');
     // and insert a document in it
     db.insert(
-    { "views": 
-    { "by_topic": 
-      {  "map": 
-      function(doc)
-     {
-      if(doc.topic)
-        {
-         emit(doc.topic, doc);
-        }
-     }
-    } 
-   }, "lists" :
-    {
-      "question_list" : 
-      function (head, req) {
+      { 
+        "views": 
+        { 
+          "by_type": 
+          {  "map": 
+            function(doc)
+            {
+              if(doc.type)
+              {
+                emit(doc.type, doc);
+              }
+            }
+          },
+          "by_project": 
+          {  "map": 
+            function(doc)
+            {
+              if(doc.project)
+              {
+                emit(doc.project, doc);
+              }
+            }
+          },
+          "average_for_question": 
+          {  "map": 
+            function(doc)
+            {
+              if(doc.type && doc.type == 'answer' && doc.question)
+              {
+                emit(doc.question, doc.answer);
+              }
+            },
+             "reduce": 
+            function(keys, values)
+            {
+              var i = 0;
+              var avg = 0.0;
+              for(var value in values)
+              {
+                avg = (avg*i +value) / (i + 1);
+                i++;
+              }
+              return avg;
+            }
+          }
+        }, 
+        "lists" :
+ {
+  "question_list" : 
+  function (head, req) {
       // specify that we're providing a JSON response
       provides('json', function() 
       {
         // create an array for our result set
         var results = [];
-
         while (row = getRow()) {
-            results.push({
-                question: row.value.question,
-                topic: row.value.topic
-            });
+          results.push({
+            question: row.value.question,
+            topic: row.value.topic
+          });
         }
-
         // make sure to stringify the results :)
-        send(JSON.stringify({"futuFeedbackItems":results}));
-      });
+      send(JSON.stringify({"futuFeedbackItems":results}));
+    });
     }
-    } 
-  }, '_design/questions', function (error, response) {
+  } 
+}, '_design/questions', function (error, response) {
   console.log("yay");
-  });
-
-  
+});
     insertbsdata(db);
-    
   });
 });
+}
 
-
+migration();
 
 
 app.get('/', function(req, res) {
   res.set('Content-Type', 'application/json');
   db.view('questions', 'by_question', ['Design'], function(err, body) {
-  if (!err) {
-	var obj = {};
-	obj['futuFeedbackItems'] = body.rows;
-  res.send(obj);
-  }
-  });
+    if (!err) {
+     var obj = {};
+     obj['futuFeedbackItems'] = body.rows;
+     res.send(obj);
+   }
+ });
 });
 
 /*
