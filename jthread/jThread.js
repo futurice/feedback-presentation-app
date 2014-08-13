620ebf8a4d92e16d080cc0b89d9351b1663d09f7
Olli-Matti Saario <olli-matti.saario@futurice.com>
2014-08-13 16:15:19 +0300

    web workers2

---
+++ jthread/jThread.js
@@ -0,0 +1,63 @@
+// Author: Alexander Cheprasov
+// Email: alexander@cheprasov.com
+
+(function(){
+	"use strict";
+
+	var jThread = window.jThread = function ( thread, done, config ) {
+
+		config = config || {
+			once : false
+		}
+
+		if ( this instanceof jThread ) return jThread(thread, done, config);
+
+		if (
+			! thread || typeof(thread) !== 'function'
+			|| ! done || typeof(done) !== 'function'
+		) return null;
+
+		if ( ! window.Worker || ! window.URL || ! window.URL.createObjectURL || ! window.Blob ) {
+
+			//return simple async function
+			var times = 0;
+			return function (/* args */) {
+				var args = Array.prototype.slice.call(arguments);
+				if ( config.once && times ) return ;
+				setTimeout(function(){
+					if ( config.once && times ) return ;
+					times += 1;
+					done ( thread.apply(thread, args), 'timer' );
+				},1);
+			}//fun
+
+		}//if
+
+		var worker = new Worker (
+				window.URL.createObjectURL(
+					new Blob(
+          
+						['self.onmessage = function ( wrk ) { importScripts("http://localhost:5000/bower_components/underscore/underscore.js"); var f = '+ Function.toString.call(thread) +'; self.postMessage( { status : "done", result : f.apply(f, wrk.data.args ) } ); '+ ( config.once ? ' self.close(); ' : '' ) +' };']
+						,{type : 'text/javascript'}
+					)
+				)
+			);
+
+		worker.onmessage = function ( wrk ) {
+			if ( typeof(wrk.data) === 'object' && wrk.data.status ) {
+				done (wrk.data.result, wrk.data.status);
+			} else {
+				done (wrk.data);
+			}//if
+		}//fun
+
+		return function (/* args */) {
+			var obj = {
+				args : Array.prototype.slice.call(arguments)
+			};
+			worker.postMessage(obj);
+		}//fun
+
+	}//fun
+
+}());
